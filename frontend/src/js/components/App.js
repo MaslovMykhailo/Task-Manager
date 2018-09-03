d32a5024587f9280d9c5740d0853b8dea9536c2f
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-09-04 00:02:02 +0300

    Create App component

---
+++ frontend/src/js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Logo from './Logo';
+import '../../css/app.css';
+
+const App = () => {
+  return (
+    <div className={'app'}>
+      <Logo />
+    </div>
+  )
+};
+
+export default App;

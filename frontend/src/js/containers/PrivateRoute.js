05388f27811bfc6d3d7e7093c0b6fb6e6d56bc08
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-09-11 10:18:28 +0300

    Create Private Route

---
+++ frontend/src/js/containers/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+
+
+const PrivateRoute = ({ component: Component, isSignedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isSignedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={'/login'}
+        />
+      )
+    }
+  />
+);
+
+const mapStateToProps = state => ({
+  isSignedIn: state.signIn.isSignedIn
+});
+
+export default connect(mapStateToProps)(PrivateRoute);

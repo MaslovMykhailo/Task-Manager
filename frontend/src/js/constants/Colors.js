20f50ce37542df43c972e217f4ad9acc828c04a7
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-09-24 20:11:00 +0300

    Create Colors obj

---
+++ frontend/src/js/constants/Colors.js
@@ -0,0 +1,50 @@
+export default {
+  blue: {
+    RGB: 'rgb(52,131,244)',
+    darkRGB: 'rgb(55,96,210)'
+  },
+  cyan: {
+    RGB: 'rgb(0,192,212)',
+    darkRGB: 'rgb(0,155,167)'
+  },
+  teal: {
+    RGB: 'rgb(0,159,136)',
+    darkRGB: 'rgb(0,129,108)'
+  },
+  green: {
+    RGB: 'rgb(0,192,82)',
+    darkRGB: 'rgb(0,156,63)'
+  },
+  yellow: {
+    RGB: 'rgb(247,250,54)',
+    darkRGB: 'rgb(254,197,36)'
+  },
+  amber: {
+    RGB: 'rgb(255,198,0)',
+    darkRGB: 'rgb(255,155,0)'
+  },
+  orange: {
+    RGB: 'rgb(255,144,0)',
+    darkRGB: 'rgb(255,108,0)'
+  },
+  brown: {
+    RGB: 'rgb(125,82,72)',
+    darkRGB: 'rgb(98,63,57)'
+  },
+  red: {
+    RGB: 'rgb(255,0,45)',
+    darkRGB: 'rgb(229,0,41)'
+  },
+  pink: {
+    RGB: 'rgb(255,0,94)',
+    darkRGB: 'rgb(213,0,88)'
+  },
+  purple: {
+    RGB: 'rgb(179,0,174)',
+    darkRGB: 'rgb(144,0,161)'
+  },
+  indigo: {
+    RGB: 'rgb(86,39,180)',
+    darkRGB: 'rgb(73,0,158)'
+  }
+}
